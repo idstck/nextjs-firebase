7e84390d5d7812a6b8743a2b33ea9a32df48a8e2
Lionatic <ahmadhakim404@gmail.com>
2021-11-22 21:28:28 +0700

    membuat fungsi submit task ke firestore

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -1,12 +1,42 @@
 import { Button, TextField } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
+import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import { db } from "../firebase";
 
 const TaskForm = () => {
+    const [task, setTask] = useState({
+        title: '',
+        detail: ''
+    })
+    const onSubmit = async () => {
+        const collectionRef = collection(db, "tasks")
+        const docRef = await addDoc(collectionRef, { ...task, timestamp: serverTimestamp() })
+        alert(`Task with id ${docRef.id} is added successfully`)
+        setTask({title: '',detail: ''})
+    }
     return (
         <div>
-            <TextField fullWidth  label="title" margin="normal"/>
-            <TextField fullWidth label="detail" multiline maxRows="{4}" margin="normal"/>
-            <Button variant="contained" sx={{ mt:3 }} endIcon={<SendIcon/>}>
+            <TextField 
+                fullWidth  
+                label="title" 
+                margin="normal"
+                value={task.title}
+                onChange={e => setTask({...task, title: e.target.value})}/>
+            <TextField 
+                fullWidth 
+                label="detail" 
+                multiline 
+                maxRows="{4}" 
+                margin="normal"
+                value={task.detail}
+                onChange={e => setTask({...task, detail: e.target.value})}/>
+            <Button 
+                variant="contained" 
+                sx={{ mt:3 }} 
+                endIcon={<SendIcon/>}
+                onClick={onSubmit}
+                >
                 Add Task 
             </Button>
         </div>
