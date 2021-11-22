8d06e0c413e72438a7ac76146fc66195bacd7725
Lionatic <ahmadhakim404@gmail.com>
2021-11-22 22:31:45 +0700

    membuat fungsi hapus data firestore

--- components/Task.js
+++ components/Task.js
@@ -1,11 +1,34 @@
-import { ListItem, ListItemText } from "@mui/material"
+import { deleteDoc, doc } from "@firebase/firestore";
+import { IconButton, ListItem, ListItemText } from "@mui/material"
 import moment from "moment"
+import { useContext } from "react";
+import { db } from "../firebase";
+import { TaskContext } from "../pages/TaskContext";
+import DeleteIcon from '@mui/icons-material/Delete'
+import MorevertIcon from '@mui/icons-material/Morevert'
 
 const Task = ({id, title, detail, timestamp}) => {
+    const { showAlert } = useContext(TaskContext)
+    const deleteTask = async(id, e) => {
+        e.stopPropagation();
+        const docRef = doc(db, "tasks", id)
+        await deleteDoc(docRef)
+        showAlert('error', `Task with id ${id} deleted successfully`)
+    }
     return (
         <ListItem
             sx={{ mt: 3, boxShadow: 3}}
             style={{ backgroundColor: '#FAFAFA'}}
+            secondaryAction={
+                <>
+                    <IconButton onClick={e => deleteTask(id, e)}>
+                        <DeleteIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MorevertIcon />
+                    </IconButton>
+                </>
+            }
         >
             <ListItemText primary={title} secondary={moment(timestamp).format("dddd, D/M/Y")}/>
         </ListItem>
