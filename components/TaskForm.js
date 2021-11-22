010da5272b838ae96aee1aae2ad4e0245495ca92
Lionatic <ahmadhakim404@gmail.com>
2021-11-22 21:15:55 +0700

    mendesain component form

---
+++ components/TaskForm.js
@@ -0,0 +1,16 @@
+import { Button, TextField } from "@mui/material"
+import SendIcon from '@mui/icons-material/Send';
+
+const TaskForm = () => {
+    return (
+        <div>
+            <TextField fullWidth  label="title" margin="normal"/>
+            <TextField fullWidth label="detail" multiline maxRows="{4}" margin="normal"/>
+            <Button variant="contained" sx={{ mt:3 }} endIcon={<SendIcon/>}>
+                Add Task 
+            </Button>
+        </div>
+    )
+}
+
+export default TaskForm
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Container } from "@mui/material";
+import TaskForm from "../components/TaskForm";
 import TaskList from "../components/TaskList";
 
 export default function Home() {
   return (
-    <Container>
+    <Container maxWidth="sm">
+      <TaskForm/>
       <TaskList/>
     </Container>
   )
